e7b314cc0184ec17692712af462e2685a7f2c1c3
Petter Kjelkenes <kjelkenes@gmail.com>
2016-05-15 12:24:31 +0200

    Start off by supporting Magento widget facebook

---
+++ Block/AsyncInit.php
@@ -0,0 +1,30 @@
+<?php
+namespace Peec\Facebook\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\Locale\ResolverInterface;
+use Magento\Framework\View\Element\Template\Context;
+
+class AsyncInit extends Template {
+
+    protected $locale;
+
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param ResolverInterface $locale
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        ResolverInterface $locale,
+        array $data
+    ) {
+        parent::__construct($context, $data);
+        $this->locale = $locale;
+    }
+    public function getLocale()
+    {
+        return $this->locale->getLocale();
+    }
+}
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2016 Pheme. All rights reserved.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Peec_Facebook',
+    __DIR__
+);
